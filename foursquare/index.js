85d499aa6613206fe760af6dc4357a8fbe3b0690
Spiros Vathis <spiros.vathis@gmail.com>
2019-07-29 22:56:21 +0300

    Adding Foursquare functionality

--- foursquare/index.js
+++ foursquare/index.js
@@ -1,10 +1,21 @@
+var
+    async = require("async"),
+    moment = require('moment'),
+    util = require('util'),
+    nodeFoursquare = require('node-foursquare'),
+    logger = require('winston'),
+    ics = require('ics'),
+    {writeFileSync } = require('fs')
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello from Azure " + (req.query.name || req.body.name)
+            body: "Hello from Azure " + (req.query.name || req.body.name) + "\n" + process.env['FOURSQUARE_ACCESS_TOKEN']
+
         };
     }
     else {

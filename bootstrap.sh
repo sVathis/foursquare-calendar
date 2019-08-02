0f8720a337220d0a9af887a47ecf33b56f0fca23
Spiros Vathis <spiros.vathis@citrix.com>
2019-08-02 17:17:20 +0300

    Updates in bootstrapping scipt

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 az group create --name svathis-calendar --location westeurope
 az storage account create --name svathiscalendar --location westeurope --resource-group svathis-calendar --sku Standard_LRS
 az functionapp create --name svathis-calendar --storage-account svathiscalendar --consumption-plan-location westeurope --resource-group svathis-calendar
-
-func init svathis-calendar --source-control --worker-runtime node
-cd svathis-calendar
+func init --source-control --worker-runtime node --language javascript
 func azure storage fetch-connection-string svathiscalendar
 func new --name foursquare -l javascript --template "Http Trigger"
-func azure functionapp publish svathis-calendar --nozip
+func new --name foursquareAll -l javascript --template "Http Trigger"
+func new --name foursquareByUear -l javascript --template "Http Trigger"
+func azure functionapp publish svathis-calendar --nozip --javascript
